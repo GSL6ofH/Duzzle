266ec8907a470a901e976ff490a506799c6a570d
GSL-6ofh <guillianlangendonk@gmail.com>
2026-04-07 15:14:32 +0200

    code so you can win

--- Duzzle/Assets/Scripts/You win.cs
+++ Duzzle/Assets/Scripts/You win.cs
@@ -3,17 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class Youwin : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player1"))
